0e274fa618e32e1acbe3a020aeb3799ccba29eb9
Ingo Muschenetz <ingo@Grace.local>
2014-12-07 13:17:44 -0800

    Removed example applications

--- Examples/Pedro/Assets/foo.js
+++
@@ -1,9 +0,0 @@
-/*
- * something cool module
- */
-
-exports.saySomething = function(_somethingCool) {
-	Ti.API.info('Something cool from foo is ' + _somethingCool);	
-};
-
-Ti.API.info('foo module loaded');
