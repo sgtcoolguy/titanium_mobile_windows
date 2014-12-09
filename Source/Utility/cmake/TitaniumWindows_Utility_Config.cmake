3ac01f8488f6eff7667081bd80a4c3f45b5770c9
garymathews <gmathews@appcelerator.com>
2014-12-08 18:11:10 -0800

    Add Utility class

---
+++ Source/Utility/cmake/TitaniumWindows_Utility_Config.cmake
@@ -0,0 +1,11 @@
+# Titanium.Utility for Windows
+# Author: Gary Mathews
+#
+# Copyright (c) 2014 by Appcelerator, Inc. All Rights Reserved.
+# Licensed under the terms of the Apache Public License.
+# Please see the LICENSE included with this distribution for details.
+
+include(CMakeFindDependencyMacro)
+find_dependency(TitaniumKit)
+
+include("${CMAKE_BINARY_DIR}/TitaniumWindows_Utility_Targets.cmake")
