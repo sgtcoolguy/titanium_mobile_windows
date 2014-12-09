5d7a70292720f29586ddb6088ced4c7fad5d38ed
Kota Iguchi <developer@infosia.co.jp>
2014-12-09 18:30:42 +0900

    Fix compile error on TitaniumWindows

--- Source/Utility/include/TitaniumWindows/Utility.hpp
+++ Source/Utility/include/TitaniumWindows/Utility.hpp
@@ -14,6 +14,7 @@
 #include <locale>
 #include <codecvt>
 #include <ppltasks.h>
+#include <unordered_map>
 
 namespace TitaniumWindows {
 	namespace Utility {
@@ -27,10 +28,57 @@ namespace TitaniumWindows {
 			return std::string(converter.to_bytes(str->Data()));
 		}
 
-    std::vector<unsigned char> GetContentFromBuffer(Windows::Storage::Streams::IBuffer^ buffer);
-    std::vector<unsigned char> GetContentFromFile(Windows::Storage::StorageFile^ file);
-    const std::string MimeTypeForExtension(std::string& path);
-    unsigned GetMSecSinceEpoch(Windows::Foundation::DateTime d);
+    static std::vector<unsigned char> GetContentFromBuffer(Windows::Storage::Streams::IBuffer^ buffer) {
+      const auto reader = Windows::Storage::Streams::DataReader::FromBuffer(buffer);
+      std::vector<unsigned char> data(reader->UnconsumedBufferLength);
+      if (!data.empty()) {
+        reader->ReadBytes(::Platform::ArrayReference<unsigned char>(&data[0], data.size()));
+      }
+      return data;
+    }
+
+    static std::vector<unsigned char> GetContentFromFile(Windows::Storage::StorageFile^ file) {
+      std::vector<unsigned char> content;
+      concurrency::event event;
+      concurrency::task<Windows::Storage::Streams::IBuffer^>(Windows::Storage::FileIO::ReadBufferAsync(file)).then([&content, &event](concurrency::task<Windows::Storage::Streams::IBuffer^> task) {
+        try {
+          content = std::move(GetContentFromBuffer(task.get()));
+        } catch (::Platform::COMException^ ex) {
+          // TODO throw JavaScript Exception
+        }
+        event.set();
+      }, concurrency::task_continuation_context::use_arbitrary());
+      event.wait();
+
+      return content;
+    }
+
+    static const std::string MimeTypeForExtension(std::string& path) {
+      const static std::unordered_map<std::string, const std::string> mimeTypeFromExtensionDict = {
+        { "css", "text/css" },
+        { "m4v", "video/x-m4v" }
+      };
+
+      const auto pos = path.find_last_of(".");
+      if (pos != std::string::npos && path.size() > pos) {
+        const auto ext = path.substr(pos + 1);
+        const auto found = mimeTypeFromExtensionDict.find(ext);
+        if (found != mimeTypeFromExtensionDict.end()) {
+          return found->second;
+        }
+      }
+      return "application/octet-stream";
+    }
+
+    static unsigned GetMSecSinceEpoch(Windows::Foundation::DateTime d) {
+      Windows::Globalization::Calendar^ cal = ref new Windows::Globalization::Calendar();
+      cal->SetDateTime(d);
+
+      std::tm tm = { cal->Second, cal->Minute, cal->Hour + (cal->Period == 1 ? 0 : 12), cal->Day, cal->Month - 1, cal->Year - 1900, 0, 0, -1 };
+      const std::time_t t = std::mktime(&tm);
+
+      return t * 1000;
+    }
 
 	} // namespace Utility
 } // namespace TitaniumWindows
