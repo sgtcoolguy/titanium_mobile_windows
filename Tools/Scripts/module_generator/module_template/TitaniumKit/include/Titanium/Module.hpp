9d2b34c1f027cdb5d40d4db81c997152b1bc5bee
garymathews <gmathews@appcelerator.com>
2014-12-12 16:07:28 -0800

    Minor template fix

--- Tools/Scripts/module_generator/module_template/TitaniumKit/include/Titanium/Module.hpp
+++ Tools/Scripts/module_generator/module_template/TitaniumKit/include/Titanium/Module.hpp
@@ -1,4 +1,7 @@
 /**
+ * Titanium.{#Module#} for Windows
+ * Author: {#Author#}
+ *
  * Copyright (c) 2014 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License.
  * Please see the LICENSE included with this distribution for details.
--- Tools/Scripts/module_generator/sub_class_template/Module/src/SubClass.cpp
+++ Tools/Scripts/module_generator/sub_class_template/Module/src/SubClass.cpp
@@ -1,6 +1,6 @@
 /**
  * Titanium.{#SubClass#} for Windows
- * Author: Gary Mathews
+ * Author: {#Author#}
  *
  * Copyright (c) 2014 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License.
