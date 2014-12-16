24380c102c1c64bf29eebf0e25d1f7c12a307f8f
Kota Iguchi <developer@infosia.co.jp>
2014-12-16 15:25:11 +0900

    Test program which demonstrates the Issue [ Module crashes at destructor #34]

---
+++ Examples/Issue34/src/Assets/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+for (var i = 0; i < 1000; i++) {
+    var b = Ti.UI.createButton({
+        top: 20,
+        bottom: 20,
+        width: 200,
+        height: Ti.UI.SIZE
+    });
+    b = null;
+}
