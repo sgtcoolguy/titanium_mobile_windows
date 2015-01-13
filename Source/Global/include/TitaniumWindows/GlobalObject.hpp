02f79f02e488c8a728e93928856c762bb4c3a777
sgtcoolguy <chris.a.williams@gmail.com>
2015-01-13 10:47:35 -0500

    Format Global module with clang-format

--- Source/Global/include/TitaniumWindows/GlobalObject.hpp
+++ Source/Global/include/TitaniumWindows/GlobalObject.hpp
@@ -11,47 +11,43 @@
 
 #include "TitaniumWindows/detail/GlobalBase.hpp"
 
-namespace TitaniumWindows {
+namespace TitaniumWindows
+{
+	using namespace HAL;
 
-using namespace HAL;
+	/*!
+	  @class
 
-/*!
-  @class
-  
-  @discussion This is the Titanium GlobalObject implementation for
-  Windows.
-*/
-class TITANIUMWINDOWS_GLOBAL_EXPORT GlobalObject final : public Titanium::GlobalObject, public JSExport < GlobalObject > {
-	
-public:
-	
-	std::string get_example_resource() const                              TITANIUM_NOEXCEPT;
-	void        set_example_resource(const std::string& example_resource) TITANIUM_NOEXCEPT;
-	
-	
-	GlobalObject(const JSContext& js_context)                                TITANIUM_NOEXCEPT;
-	GlobalObject(const GlobalObject&, const std::vector<JSValue>& arguments) TITANIUM_NOEXCEPT;
-	
-	virtual ~GlobalObject() = default;
-	GlobalObject(const GlobalObject&) = default;
-	GlobalObject& operator=(const GlobalObject&) = default;
+	  @discussion This is the Titanium GlobalObject implementation for
+	  Windows.
+	*/
+	class TITANIUMWINDOWS_GLOBAL_EXPORT GlobalObject final : public Titanium::GlobalObject, public JSExport<GlobalObject>
+	{
+	public:
+		std::string get_example_resource() const TITANIUM_NOEXCEPT;
+		void set_example_resource(const std::string& example_resource) TITANIUM_NOEXCEPT;
+
+		GlobalObject(const JSContext& js_context) TITANIUM_NOEXCEPT;
+		GlobalObject(const GlobalObject&, const std::vector<JSValue>& arguments) TITANIUM_NOEXCEPT;
+
+		virtual ~GlobalObject() = default;
+		GlobalObject(const GlobalObject&) = default;
+		GlobalObject& operator=(const GlobalObject&) = default;
 #ifdef TITANIUM_MOVE_CTOR_AND_ASSIGN_DEFAULT_ENABLE
-	GlobalObject(GlobalObject&&)                 = default;
-	GlobalObject& operator=(GlobalObject&&)      = default;
+		GlobalObject(GlobalObject&&) = default;
+		GlobalObject& operator=(GlobalObject&&) = default;
 #endif
-	
-	static void JSExportInitialize();
-	
-protected:
-	
-	virtual std::string LoadResource(const std::string& moduleId) const TITANIUM_NOEXCEPT override final;
-	virtual std::shared_ptr<Titanium::GlobalObject::Timer> CreateTimer(Callback_t callback, const std::chrono::milliseconds& interval) const TITANIUM_NOEXCEPT override final;
-	
-private:
-	
-	std::string example_resource__;
-};
+
+		static void JSExportInitialize();
+
+	protected:
+		virtual std::string LoadResource(const std::string& moduleId) const TITANIUM_NOEXCEPT override final;
+		virtual std::shared_ptr<Titanium::GlobalObject::Timer> CreateTimer(Callback_t callback, const std::chrono::milliseconds& interval) const TITANIUM_NOEXCEPT override final;
+
+	private:
+		std::string example_resource__;
+	};
 
 }  // namespace TitaniumWindows {
 
-#endif // _TITANIUMWINDOWS_GLOBALOBJECT_HPP_
+#endif  // _TITANIUMWINDOWS_GLOBALOBJECT_HPP_
