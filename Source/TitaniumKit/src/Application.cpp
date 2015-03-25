309afbf6d13c8032e30364f1bd7a72d397cba305
Kota Iguchi <developer@infosia.co.jp>
2015-03-25 18:18:48 +0900

    Implement "Application Error" dialog

--- Source/TitaniumKit/src/Application.cpp
+++ Source/TitaniumKit/src/Application.cpp
@@ -19,9 +19,73 @@ namespace Titanium
 
 	JSValue Application::Run(const std::string& app_js)
 	{
-		std::ostringstream os;
-		os << "require('" << app_js << "');\n";
-		return js_context__.JSEvaluateScript(os.str());
+		std::string show_error_js = R"js(
+/**
+ * Creates the Red Screen Of Death!
+ * @param (e) Error or Exception
+ */
+function Titanium_RedScreenOfDeath(e) {
+    try {
+        Ti.API.error(e);
+
+        var f = e.filename || "",
+            match = f.match(/:\/\/.+(\/.*)/),
+            filename = match ? match[1] : e.filename,
+            line = e.lineno,
+            win = Ti.UI.createWindow({
+                backgroundColor: "#f00",
+                layout: "vertical"
+            }),
+            view,
+            button;
+
+        function makeLabel(text, height, color, fontSize) {
+            win.add(Ti.UI.createLabel({
+                color: color,
+                font: { fontSize: fontSize, fontWeight: "bold" },
+                height: height,
+                width: Ti.UI.FILL,
+                textAlign: 'center', //UI.TEXT_ALIGNMENT_CENTER,
+                text: text
+            }));
+        }
+
+        win.add(view = Ti.UI.createView({ height: "12%" }));
+        makeLabel("Application Error", "15%", "#0f0", "34");
+        makeLabel((e.message || e.toString()).trim() + (filename && filename !== "undefined" ? " at " + filename : "") + (line ? " (line " + line + ")" : ""), "45%", "#fff", "26");
+        win.add(view = Ti.UI.createView({ height: "12%" }));
+        view.add(button = Ti.UI.createButton({ title: "Dismiss" }));
+        button.addEventListener("click", function () {
+            win.close();
+        });
+
+        makeLabel("Error messages will only be displayed during development. When your app is packaged for final distribution, no error screen will appear. Test your code!", "28%", "#000", "20");
+
+        win.open();
+    } catch (er) {
+        Ti.API.error(er);
+    }
+}
+  )js";
+
+		js_context__.JSEvaluateScript(show_error_js);
+
+		try {
+
+			std::ostringstream os;
+			os << "try {\n";
+			os << "  require('" << app_js << "');\n";
+			os << "} catch (E) {\n";
+			os << "  Titanium_RedScreenOfDeath(E);\n";
+			os << "}";
+
+			return js_context__.JSEvaluateScript(os.str());
+
+		} catch (std::exception& stdex) {
+			const std::string what = stdex.what();
+			TITANIUM_LOG_ERROR(what);
+			return js_context__.JSEvaluateScript("Titanium_RedScreenOfDeath('" + what + "');");
+		}
 	}
 
 	JSContext Application::get_context() const
