9327d7d6e8ef2a00299217baeedfeee717b9a058
sgtcoolguy <chris.a.williams@gmail.com>
2015-05-04 10:36:06 -0400

    Latest generated files

---
+++ Source/UI/include/Platform.Object.hpp
@@ -0,0 +1,57 @@
+/**
+ * Windows Native Wrapper for Platform.Object
+ *
+ * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License.
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#ifndef _PLATFORM_OBJECT_HPP_
+#define _PLATFORM_OBJECT_HPP_
+
+#include "TitaniumWindows/UI/detail/UIBase.hpp"
+#include "TitaniumWindows/Utility.hpp"
+#include "Titanium/Module.hpp"
+
+namespace Titanium
+{
+	namespace Platform
+	{
+
+
+		using namespace HAL;
+
+		class TITANIUMWINDOWS_UI_EXPORT Object : public ::Titanium::Module, public JSExport<Object>
+		{
+
+		public:
+
+
+			Object(const JSContext&) TITANIUM_NOEXCEPT;
+
+			virtual ~Object() = default;
+			Object(const Object&) = default;
+			Object& operator=(const Object&) = default;
+#ifdef TITANIUM_MOVE_CTOR_AND_ASSIGN_DEFAULT_ENABLE
+			Object(Object&&)                 = default;
+			Object& operator=(Object&&)      = default;
+#endif
+
+			static void JSExportInitialize();
+
+			virtual void postCallAsConstructor(const JSContext& js_context, const std::vector<JSValue>& arguments) override;
+
+			::Platform::Object^ unwrapPlatform_Object() const;
+			void wrap(::Platform::Object^ object);
+
+		protected:
+			::Platform::Object^ wrapped__;
+
+		private:
+			::Platform::Object^ unwrap() const;
+
+		};
+
+	} // namespace Platform
+} // namespace Titanium
+#endif // _PLATFORM_OBJECT_HPP_
