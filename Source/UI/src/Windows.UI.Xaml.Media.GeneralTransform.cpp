6251e777457b0e273eb0c70df0aa07719394e804
sgtcoolguy <chris.a.williams@gmail.com>
2015-05-05 09:52:54 -0400

    Remove generated files for now

--- Source/UI/src/Windows.UI.Xaml.Media.GeneralTransform.cpp
+++
@@ -1,175 +0,0 @@
-/**
- * Windows Native Wrapper for Windows.UI.Xaml.Media.GeneralTransform
- *
- * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License.
- * Please see the LICENSE included with this distribution for details.
- */
-
-#include "Windows.UI.Xaml.Media.GeneralTransform.hpp"
-
-namespace Titanium
-{
-	namespace Windows
-	{
-		namespace UI
-		{
-			namespace Xaml
-			{
-				namespace Media
-				{
-
-		GeneralTransform::GeneralTransform(const JSContext& js_context) TITANIUM_NOEXCEPT
-			: Windows::UI::Xaml::DependencyObject(js_context)
-		{
-		}
-
-		void GeneralTransform::postCallAsConstructor(const JSContext& js_context, const std::vector<JSValue>& arguments)
-		{	
-			// TODO Handle passing along args to the constructor. Not all items have default constructor!
-		}
-
-		::Windows::UI::Xaml::Media::GeneralTransform^ GeneralTransform::unwrapWindows_UI_Xaml_Media_GeneralTransform() const
-		{
-			return dynamic_cast<::Windows::UI::Xaml::Media::GeneralTransform^>(wrapped__);
-		}
-
-		::Windows::UI::Xaml::Media::GeneralTransform^ GeneralTransform::unwrap() const
-		{
-			return unwrapWindows_UI_Xaml_Media_GeneralTransform();
-		}
-
-		void GeneralTransform::wrap(::Windows::UI::Xaml::Media::GeneralTransform^ object)
-		{
-			wrapped__ = object;
-		}
-
-		void GeneralTransform::JSExportInitialize()
-		{
-			JSExport<GeneralTransform>::SetClassVersion(1);
-			JSExport<GeneralTransform>::SetParent(JSExport<Windows::UI::Xaml::DependencyObject>::Class());
-
-			TITANIUM_ADD_PROPERTY_READONLY(GeneralTransform, Inverse);
-			TITANIUM_ADD_PROPERTY_READONLY(GeneralTransform, InverseCore);
-			TITANIUM_ADD_FUNCTION(GeneralTransform, TransformPoint);
-			TITANIUM_ADD_FUNCTION(GeneralTransform, TryTransform);
-			TITANIUM_ADD_FUNCTION(GeneralTransform, TransformBounds);
-		}
-
-		TITANIUM_PROPERTY_GETTER(GeneralTransform, Inverse)
-		{
-			auto value = unwrap()->Inverse;
-			auto context = get_context();
-			// FIXME We're assuming the value is the exact type defined in the return type. It may be a subclass and we'll lose that detail here...
-			// I'm not sure how we can avoid it, though
-			auto result = context.CreateObject(JSExport<Windows::UI::Xaml::Media::GeneralTransform>::Class());
-			auto result_wrapper = result.GetPrivate<Windows::UI::Xaml::Media::GeneralTransform>();
-			result_wrapper->wrap(value);
-
-			return result;
-		}
-
-		TITANIUM_PROPERTY_GETTER(GeneralTransform, InverseCore)
-		{
-			auto value = unwrap()->InverseCore;
-			auto context = get_context();
-			// FIXME We're assuming the value is the exact type defined in the return type. It may be a subclass and we'll lose that detail here...
-			// I'm not sure how we can avoid it, though
-			auto result = context.CreateObject(JSExport<Windows::UI::Xaml::Media::GeneralTransform>::Class());
-			auto result_wrapper = result.GetPrivate<Windows::UI::Xaml::Media::GeneralTransform>();
-			result_wrapper->wrap(value);
-
-			return result;
-		}
-
-		TITANIUM_FUNCTION(GeneralTransform, TransformPoint)
-		{
-			auto _0 = arguments.at(0);
-			TITANIUM_ASSERT_AND_THROW(_0.IsObject(), "Expected Object");
-			auto object_point = static_cast<JSObject>(_0);
-			auto point = ::Windows::Foundation::Point(0, 0);
-			// Assign fields explicitly since we cheated on constructor args
-			auto X = object_point.GetProperty("X");
-			point.X = static_cast<double>(X);
-			auto Y = object_point.GetProperty("Y");
-			point.Y = static_cast<double>(Y);
-
-
-			auto context = get_context();
-			auto method_result = unwrap()->TransformPoint(point);
-
-			auto result = context.CreateObject();
-			result.SetProperty("X", context.CreateNumber(static_cast<double>(method_result.X)));
-			result.SetProperty("Y", context.CreateNumber(static_cast<double>(method_result.Y)));
-
-			return result;
-		}
-
-		TITANIUM_FUNCTION(GeneralTransform, TryTransform)
-		{
-			auto _0 = arguments.at(0);
-			TITANIUM_ASSERT_AND_THROW(_0.IsObject(), "Expected Object");
-			auto object_inPoint = static_cast<JSObject>(_0);
-			auto inPoint = ::Windows::Foundation::Point(0, 0);
-			// Assign fields explicitly since we cheated on constructor args
-			auto X = object_inPoint.GetProperty("X");
-			inPoint.X = static_cast<double>(X);
-			auto Y = object_inPoint.GetProperty("Y");
-			inPoint.Y = static_cast<double>(Y);
-
-
-			auto _1 = arguments.at(1);
-			TITANIUM_ASSERT_AND_THROW(_1.IsObject(), "Expected Object");
-			auto object_outPoint = static_cast<JSObject>(_1);
-			auto outPoint = ::Windows::Foundation::Point(0, 0);
-			// Assign fields explicitly since we cheated on constructor args
-			auto X = object_outPoint.GetProperty("X");
-			outPoint.X = static_cast<double>(X);
-			auto Y = object_outPoint.GetProperty("Y");
-			outPoint.Y = static_cast<double>(Y);
-
-
-			auto context = get_context();
-			auto method_result = unwrap()->TryTransform(inPoint, &outPoint);
-
- 			auto result = context.CreateBoolean(method_result); 
-
-			_0 = _0.get_context().CreateNumber(outPoint);
-			_1 = _1.get_context().CreateNumber(outPoint);
-			return result;
-		}
-
-		TITANIUM_FUNCTION(GeneralTransform, TransformBounds)
-		{
-			auto _0 = arguments.at(0);
-			TITANIUM_ASSERT_AND_THROW(_0.IsObject(), "Expected Object");
-			auto object_rect = static_cast<JSObject>(_0);
-			auto rect = ::Windows::Foundation::Rect(0, 0, 0, 0);
-			// Assign fields explicitly since we cheated on constructor args
-			auto X = object_rect.GetProperty("X");
-			rect.X = static_cast<double>(X);
-			auto Y = object_rect.GetProperty("Y");
-			rect.Y = static_cast<double>(Y);
-			auto Width = object_rect.GetProperty("Width");
-			rect.Width = static_cast<double>(Width);
-			auto Height = object_rect.GetProperty("Height");
-			rect.Height = static_cast<double>(Height);
-
-
-			auto context = get_context();
-			auto method_result = unwrap()->TransformBounds(rect);
-
-			auto result = context.CreateObject();
-			result.SetProperty("X", context.CreateNumber(static_cast<double>(method_result.X)));
-			result.SetProperty("Y", context.CreateNumber(static_cast<double>(method_result.Y)));
-			result.SetProperty("Width", context.CreateNumber(static_cast<double>(method_result.Width)));
-			result.SetProperty("Height", context.CreateNumber(static_cast<double>(method_result.Height)));
-
-			return result;
-		}
-
-				} // namespace Media
-			} // namespace Xaml
-		} // namespace UI
-	} // namespace Windows
-} // namespace Titanium
--- Source/UI/src/Windows.UI.Xaml.Media.SolidColorBrush.cpp
+++
@@ -1,107 +0,0 @@
-/**
- * Windows Native Wrapper for Windows.UI.Xaml.Media.SolidColorBrush
- *
- * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License.
- * Please see the LICENSE included with this distribution for details.
- */
-
-#include "Windows.UI.Xaml.DependencyProperty.hpp"
-#include "Windows.UI.Xaml.Media.SolidColorBrush.hpp"
-
-namespace Titanium
-{
-	namespace Windows
-	{
-		namespace UI
-		{
-			namespace Xaml
-			{
-				namespace Media
-				{
-
-		SolidColorBrush::SolidColorBrush(const JSContext& js_context) TITANIUM_NOEXCEPT
-			: Windows::UI::Xaml::Media::Brush(js_context)
-		{
-		}
-
-		void SolidColorBrush::postCallAsConstructor(const JSContext& js_context, const std::vector<JSValue>& arguments)
-		{	
-			// TODO Handle passing along args to the constructor. Not all items have default constructor!
-		}
-
-		::Windows::UI::Xaml::Media::SolidColorBrush^ SolidColorBrush::unwrapWindows_UI_Xaml_Media_SolidColorBrush() const
-		{
-			return dynamic_cast<::Windows::UI::Xaml::Media::SolidColorBrush^>(wrapped__);
-		}
-
-		::Windows::UI::Xaml::Media::SolidColorBrush^ SolidColorBrush::unwrap() const
-		{
-			return unwrapWindows_UI_Xaml_Media_SolidColorBrush();
-		}
-
-		void SolidColorBrush::wrap(::Windows::UI::Xaml::Media::SolidColorBrush^ object)
-		{
-			wrapped__ = object;
-		}
-
-		void SolidColorBrush::JSExportInitialize()
-		{
-			JSExport<SolidColorBrush>::SetClassVersion(1);
-			JSExport<SolidColorBrush>::SetParent(JSExport<Windows::UI::Xaml::Media::Brush>::Class());
-
-			TITANIUM_ADD_PROPERTY(SolidColorBrush, Color);
-			TITANIUM_ADD_PROPERTY_READONLY(SolidColorBrush, ColorProperty);
-		}
-
-		TITANIUM_PROPERTY_SETTER(SolidColorBrush, Color)
-		{
-			TITANIUM_ASSERT_AND_THROW(argument.IsObject(), "Expected Object");
-			auto object_value = static_cast<JSObject>(argument);
-			auto value = ::Windows::UI::Color(0, 0, 0, 0);
-			// Assign fields explicitly since we cheated on constructor args
-			auto A = object_value.GetProperty("A");
-			value.A = static_cast<int32_t>(A);
-			auto R = object_value.GetProperty("R");
-			value.R = static_cast<int32_t>(R);
-			auto G = object_value.GetProperty("G");
-			value.G = static_cast<int32_t>(G);
-			auto B = object_value.GetProperty("B");
-			value.B = static_cast<int32_t>(B);
-
-
-			unwrap()->Color = value;
-			return true;
-		}
-
-		TITANIUM_PROPERTY_GETTER(SolidColorBrush, Color)
-		{
-			auto value = unwrap()->Color;
-			auto context = get_context();
-			auto result = context.CreateObject();
-			result.SetProperty("A", context.CreateNumber(static_cast<int32_t>(value.A)));
-			result.SetProperty("R", context.CreateNumber(static_cast<int32_t>(value.R)));
-			result.SetProperty("G", context.CreateNumber(static_cast<int32_t>(value.G)));
-			result.SetProperty("B", context.CreateNumber(static_cast<int32_t>(value.B)));
-
-			return result;
-		}
-
-		TITANIUM_PROPERTY_GETTER(SolidColorBrush, ColorProperty)
-		{
-			auto value = unwrap()->ColorProperty;
-			auto context = get_context();
-			// FIXME We're assuming the value is the exact type defined in the return type. It may be a subclass and we'll lose that detail here...
-			// I'm not sure how we can avoid it, though
-			auto result = context.CreateObject(JSExport<Windows::UI::Xaml::DependencyProperty>::Class());
-			auto result_wrapper = result.GetPrivate<Windows::UI::Xaml::DependencyProperty>();
-			result_wrapper->wrap(value);
-
-			return result;
-		}
-
-				} // namespace Media
-			} // namespace Xaml
-		} // namespace UI
-	} // namespace Windows
-} // namespace Titanium
