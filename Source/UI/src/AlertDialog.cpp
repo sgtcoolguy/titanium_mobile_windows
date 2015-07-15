05af1265875e74c9a746255b65e58a356da5973a
Kota Iguchi <developer@infosia.co.jp>
2015-07-15 11:09:57 +0900

    Fix: AlertDialog is not shown more than twice

--- Source/UI/src/AlertDialog.cpp
+++ Source/UI/src/AlertDialog.cpp
@@ -18,7 +18,7 @@ namespace TitaniumWindows
 		using namespace Windows::UI::Popups;
 		using namespace Windows::Foundation;
 
-		std::vector<Windows::UI::Popups::MessageDialog^> AlertDialog::dialogs__;
+		std::vector<Windows::UI::Popups::MessageDialog^> AlertDialog::dialog_queue__;
 
 		AlertDialog::AlertDialog(const JSContext& js_context) TITANIUM_NOEXCEPT
 		    : Titanium::UI::AlertDialog(js_context)
@@ -39,8 +39,8 @@ namespace TitaniumWindows
 
 		void AlertDialog::show() TITANIUM_NOEXCEPT
 		{
-			std::string title   = get_title();
-			std::string message = get_message();
+			const std::string title   = get_title();
+			const std::string message = get_message();
 
 			Windows::UI::Popups::MessageDialog^ dialog = ref new Windows::UI::Popups::MessageDialog(TitaniumWindows::Utility::ConvertUTF8String(message), TitaniumWindows::Utility::ConvertUTF8String(title));
 			dialog->DefaultCommandIndex = 0;
@@ -52,14 +52,14 @@ namespace TitaniumWindows
 
 			auto maxButtons = buttonNames__.size();
 			if (buttonNames__.size() > MaxButtonCount) {
-				auto Titanium_property = get_context().get_global_object().GetProperty("Titanium");
+				const auto Titanium_property = get_context().get_global_object().GetProperty("Titanium");
 				TITANIUM_ASSERT(Titanium_property.IsObject());  // precondition
-				auto Titanium = static_cast<JSObject>(Titanium_property);
+				const auto Titanium = static_cast<JSObject>(Titanium_property);
 
-				auto Object_property = Titanium.GetProperty("API");
+				const auto Object_property = Titanium.GetProperty("API");
 				TITANIUM_ASSERT(Object_property.IsObject());  // precondition
-				auto api = static_cast<JSObject>(Object_property);
-				auto native_api = api.GetPrivate<Titanium::API>();
+				const auto api = static_cast<JSObject>(Object_property);
+				const auto native_api = api.GetPrivate<Titanium::API>();
 				native_api->error("Number of buttons exceeds platform maximum (" + std::to_string(MaxButtonCount) + "), list will be truncated.");
 				maxButtons = MaxButtonCount;
 			}
@@ -68,31 +68,35 @@ namespace TitaniumWindows
 			}
 
 			try {
-				dialogs__.push_back(dialog);
-				if (dialogs__.size() == 1) {
-					static std::function<void(IUICommand^)> on_click;
-					on_click = [this](IUICommand^ command) {
-						std::int32_t index = 0;
-						if (command != nullptr) {
-							auto casted = dynamic_cast<IPropertyValue^>(command->Id);
-							if (casted != nullptr) {
-								index = casted->GetInt32();
-							}
+				const static std::function<void(IUICommand^)> on_click = [this](IUICommand^ command) {
+					std::int32_t index = 0;
+					if (command != nullptr) {
+						const auto casted = dynamic_cast<IPropertyValue^>(command->Id);
+						if (casted != nullptr) {
+							index = casted->GetInt32();
 						}
-						const JSContext ctx = get_context();
-						JSObject eventArgs = ctx.CreateObject();
-						eventArgs.SetProperty("index", ctx.CreateNumber(index));
-						eventArgs.SetProperty("cancel", ctx.CreateBoolean(index == get_cancel()));
-						fireEvent("click", eventArgs);
+					}
+					const JSContext ctx = get_context();
+					JSObject eventArgs = ctx.CreateObject();
+					eventArgs.SetProperty("index", ctx.CreateNumber(index));
+					eventArgs.SetProperty("cancel", ctx.CreateBoolean(index == get_cancel()));
+					fireEvent("click", eventArgs);
 
-						// display next dialog in queue
-						if (dialogs__.size() > 1) {
-							dialogs__.erase(dialogs__.begin());
-							concurrency::create_task(dialogs__.front()->ShowAsync()).then(on_click);
-						}
-					};
-					concurrency::create_task(dialog->ShowAsync()).then(on_click);
+					dialog_queue__.erase(dialog_queue__.begin());
+
+					if (dialog_queue__.size() > 0) {
+						concurrency::create_task(dialog_queue__.at(0)->ShowAsync()).then(on_click);
+					}
+
+				};
+
+				dialog_queue__.push_back(dialog);
+
+				// show first dialog and then proceed to next after finishing click event
+				if (dialog_queue__.size() == 1) {
+					concurrency::create_task(dialog_queue__.at(0)->ShowAsync()).then(on_click);
 				}
+
 			} catch (::Platform::COMException^ ce) {
 				// Typically would have happened on phone if we supplied more than max buttons allowed
 				detail::ThrowRuntimeError("Ti.UI.AlertDialog", "Exception during show(): " + Utility::ConvertUTF8String(ce->Message));
