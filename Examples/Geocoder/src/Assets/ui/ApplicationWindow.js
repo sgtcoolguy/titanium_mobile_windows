64542c8eb2f30ed94d7691aa97bb5aa4aa73c36c
garymathews <gmathews@appcelerator.com>
2015-07-15 14:29:00 -0700

    [TIMOB-19055] Add Geocoder example

---
+++ Examples/Geocoder/src/Assets/ui/ApplicationWindow.js
@@ -0,0 +1,132 @@
+if (Titanium.Platform.osname != 'mobileweb') {
+	Ti.Map = require('ti.map');
+}
+
+//Application Window Component Constructor
+exports.ApplicationWindow = function() {
+function isiOS7Plus()
+{
+	// iOS-specific test
+	if (Titanium.Platform.name == 'iPhone OS')
+	{
+		var version = Titanium.Platform.version.split(".");
+		var major = parseInt(version[0],10);
+
+		// Can only test this support on a 3.2+ device
+		if (major >= 7)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+var isWindows = (Ti.Platform.osname === "windowsphone" || Ti.Platform.osname === "windowsstore");
+
+var iOS7 = isiOS7Plus();
+var theTop = iOS7 ? 20 : 0;
+
+	var geo = require('geo');
+
+	//create object instance
+	var self = Ti.UI.createWindow({
+		backgroundColor: '#800'
+	});
+
+	// create UI components
+	var view = Ti.UI.createView({
+		backgroundColor: '#800',
+		top: theTop
+	});
+	var textfield = Ti.UI.createTextField({
+		height: isWindows ? '22.5dp' : '40dp',
+		top: isWindows ? '2.5dp' : '5dp',
+		left: '5dp',
+		right: isWindows ? '25dp' : '50dp',
+		style: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		hintText: 'Enter an address',
+		backgroundColor: '#fff',
+		paddingLeft: '5dp'
+	});
+	var button = Ti.UI.createButton({
+		title: '+',
+		font: {
+			fontSize: '20dp',
+			fontWeight: 'bold'
+		},
+		top: isWindows ? '2.5dp' : '5dp',
+		height: isWindows ? '20dp' : '40dp',
+		width: isWindows ? '20dp' : '40dp',
+		right: '5dp'
+	});
+	var mapview;
+
+	// add map after window opens
+	self.addEventListener('open', function() {
+		// Make sure we only add the map once
+		if (mapview !== undefined) {
+			return;
+		}
+
+		mapview = Titanium.Map.createView({
+			mapType: Titanium.Map.NORMAL_TYPE,
+			region: {
+				latitude: geo.LATITUDE_BASE,
+				longitude: geo.LONGITUDE_BASE,
+				latitudeDelta: 0.1,
+				longitudeDelta: 0.1
+			},
+			animate: true,
+			regionFit: true,
+			userLocation: false,
+			top: isWindows ? '40dp' : '70dp'
+		});
+
+		// Add initial annotation
+		mapview.addAnnotation(Ti.Map.createAnnotation({
+			animate: true,
+			pincolor: Titanium.Map.ANNOTATION_RED,
+			title: 'Appcelerator',
+			latitude: geo.LATITUDE_BASE,
+			longitude: geo.LONGITUDE_BASE,
+			leftButton: '/images/delete.png'
+		}));
+
+		// Handle all map annotation clicks
+		mapview.addEventListener('click', function(e) {
+			if (e.annotation && (e.clicksource === 'leftButton' || e.clicksource == 'leftPane')) {
+				mapview.removeAnnotation(e.annotation);
+			}
+		});
+		self.add(mapview);
+	});
+
+
+	// Execute forward geocode on button click
+	button.addEventListener('click', function() {
+		textfield.blur();
+		geo.forwardGeocode(textfield.value, function(geodata) {
+			mapview.addAnnotation(Ti.Map.createAnnotation({
+				animate: true,
+				pincolor: Titanium.Map.ANNOTATION_RED,
+				title: geodata.title,
+				latitude: geodata.coords.latitude,
+				longitude: geodata.coords.longitude,
+				leftButton: '/images/delete.png'
+			}));
+			mapview.setLocation({
+				latitude: geodata.coords.latitude,
+				longitude: geodata.coords.longitude,
+				latitudeDelta: 1,
+				longitudeDelta: 1
+			});
+		});
+	});
+
+	// assemble view hierarchy
+	view.add(textfield);
+	view.add(button);
+	self.add(view);
+
+	return self;
+};
