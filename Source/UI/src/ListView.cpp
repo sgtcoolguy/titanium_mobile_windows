a98cae1cbd4749fe3959bb32d5a26555ea30d798
Ewan Harris <ewanharris93@gmail.com>
2017-07-10 21:50:06 +0100

    [TIMOB-24942] Fix scrolling of ListView with height set to auto (#1037)

--- Source/UI/src/ListView.cpp
+++ Source/UI/src/ListView.cpp
@@ -73,6 +73,8 @@ namespace TitaniumWindows
 			Titanium::UI::ListView::setLayoutDelegate<WindowsViewLayoutDelegate>();
 			layoutDelegate__->set_defaultWidth(Titanium::UI::LAYOUT::FILL);
 			layoutDelegate__->set_defaultHeight(Titanium::UI::LAYOUT::FILL);
+			layoutDelegate__->set_autoLayoutForHeight(Titanium::UI::LAYOUT::FILL);
+			layoutDelegate__->set_autoLayoutForWidth(Titanium::UI::LAYOUT::FILL);
 
 			getViewLayoutDelegate<WindowsViewLayoutDelegate>()->setComponent(parent__, listview__);
 		}
