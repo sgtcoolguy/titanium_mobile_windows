3b8b5bcad213862332e4048649ed6618fc1c86d9
Kota Iguchi <developer@infosia.co.jp>
2017-11-08 00:03:04 +0900

    [TIMOB-25233] Implement TextField.setSelection (#1134)
    * [TIMOB-25233] Implement TextField.setSelection
    
    * - Reverse the range if start > end, ignore invalid range

--- Source/TitaniumKit/include/Titanium/UI/TextField.hpp
+++ Source/TitaniumKit/include/Titanium/UI/TextField.hpp
@@ -267,6 +267,18 @@ namespace Titanium
 			*/
 			virtual bool hasText() TITANIUM_NOEXCEPT;
 
+			/*!
+			  @method
+
+			  @abstract setSelection() : void
+
+			  @discussion Selects the text in range (start, end).
+
+			  @result void
+			*/
+			virtual void setSelection(const std::uint32_t& start, const std::uint32_t& end) TITANIUM_NOEXCEPT;
+
+
 			TextField(const JSContext&) TITANIUM_NOEXCEPT;
 
 			virtual ~TextField() = default;
@@ -370,6 +382,8 @@ namespace Titanium
 			TITANIUM_FUNCTION_DEF(blur);
 			TITANIUM_FUNCTION_DEF(focus);
 			TITANIUM_FUNCTION_DEF(hasText);
+
+			TITANIUM_FUNCTION_DEF(setSelection);
 
 		private:
 #pragma warning(push)
